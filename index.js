c9eeab4050e9ad240856541165bc8f58ddd1bd25
omkar <omkarbandal00@gmail.com>
2025-06-10 10:06:12 +0530

    changed home page text

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>Good Morning,Omkar<h1>");
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.send("<h1>About Page<h1>");
 });
 
 app.get("/contact", (req, res) => {
